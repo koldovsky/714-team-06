a7f5655d5c8b3a54515ce2f2c30da3957bed497b
Ira1234567 <iraowl1234@gmail.com>
2022-09-28 10:24:29 +0000

    Add Next brand carousel button function

--- js/special-offers-carousel.js
+++ js/special-offers-carousel.js
@@ -1,13 +1,13 @@
 (function() {
 
     const brandSlides = [
-        '<div><img scr="/workspace/714-team-06/img/aquarium.png" alt="Applaws logo"></div>',
-        '<div><img scr="img/docas.png" alt="Docas logo"></div>',
-        '<div><img scr="img/felix.png" alt="Felix logo"></div>',
-        '<div><img scr="img/gourmet.png" alt="Gourmet logo"></div>',
-        '<div><img scr="img/purizon.png" alt="Purizon logo"></div>',
-        '<div><img scr="img/rocco.png" alt="Rocco logo"></div>',
-        '<div><img scr="img/tigerino.png" alt="Tigerino logo"></div>',
+        '<div><img src="img/applaws.png" alt="Applaws logo"></div>',
+        '<div><img src="img/docas.png" alt="Docas logo"></div>',
+        '<div><img src="img/felix.png" alt="Felix logo"></div>',
+        '<div><img src="img/gourmet.png" alt="Gourmet logo"></div>',
+        '<div><img src="img/purizon.png" alt="Purizon logo"></div>',
+        '<div><img src="img/rocco.png" alt="Rocco logo"></div>',
+        '<div><img src="img/tigerino.png" alt="Tigerino logo"></div>',
     ];
 
     let currentBrandSlideIdx = 0;
@@ -23,8 +23,12 @@
         renderBrandSlide()
     }
     
+   renderBrandSlide() 
 
+/*
+    setInterval(nextBrandSlide, 1000);*/
 
-    setInterval(nextBrandSlide, 1000);
+    const brandBtnNext = document.querySelector('.brands .brand-btn-next');
+    brandBtnNext.addEventListener('click', nextBrandSlide)
 
 })();
