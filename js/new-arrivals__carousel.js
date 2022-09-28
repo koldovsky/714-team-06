6c9b4f1c56ce76fa7410f36d98293a2d29ee69fb
Oleksandra Shkoda <shkoda.aleksandra@gmail.com>
2022-09-28 11:00:31 +0100

    new arrivals js cards rendered

--- js/new-arrivals__carousel.js
+++ js/new-arrivals__carousel.js
@@ -1,6 +1,66 @@
-// (function ){
-//     const slides = 
-//     `<>`
-
-
-// }()
+  (function () {
+    const products = [
+      {
+        id: 1,
+        img: "img/img__new_arrivals/woven_toy.png",
+        title: "DUNA Woven Toy",
+        price: '1,50',
+      },
+      {
+        id: 2,
+        img: "img/img__new_arrivals/butterfly_toy.png",
+        title: "DUNA EGO Butterfly Toy",
+        price: '8,00',
+      },
+      {
+        id: 3,
+        img: "img/img__new_arrivals/plastic_bowl.png",
+        title: "OPTY Plastic Bowl",
+        price: '2,00',
+      },
+      {
+        id: 4,
+        img: "img/img__new_arrivals/woven_leash.png",
+        title: "SERA Woven Leash",
+        price: '2,00',
+      },
+      {
+        id: 5,
+        img: "img/img__new_arrivals/crunch_sticks_treat.png",
+        title: "VITALIFE Crunch Sticks Treat",
+        price: '5,00',
+      },
+    ];
+  
+    {
+      /* <svg class="arrow" width="14" height="26" viewBox="0 0 14 26" fill="none"
+  xmlns="http://www.w3.org/2000/svg">
+  <path d="M12.5 2L1.5 13L12.5 24" stroke="#464C5C" stroke-width="2" stroke-miterlimit="10"
+      stroke-linecap="square"></path>
+  </svg> */
+    }
+    function renderProducts(products) {
+      let productsContainer = " ";
+      productsContainer = document.querySelector(".arrivals_cards_container");
+  
+      for (const product of products) {
+        productsContainer.innerHTML += `    
+      
+  <article class="arrivals_cards">
+      <a href="product.html"><img class="new_arrivals_photos"
+              src=${product.img} alt="${product.title}" width="329"></a>
+      <a href="product.html" class="arrivals_cards_name">${product.title}</a>
+      <p class="price">$${product.price}</p>
+      <button class="button" type="submit">Add to Card</button>
+  </article>`;
+      }
+    }
+    /* <svg class="arrow" width="14" height="26" viewBox="0 0 14 26" fill="none"
+  // xmlns="http://www.w3.org/2000/svg">
+  // <path d="M1.5 24L12.5 13L1.5 2" stroke="#464C5C" stroke-width="2" stroke-miterlimit="10"
+  //     stroke-linecap="square"></path>
+  // </svg> */
+  
+    renderProducts(products);
+  })();
+  
