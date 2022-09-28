cde5dd4cdc22188c04ca6d4847105c00d303cc06
Ira1234567 <iraowl1234@gmail.com>
2022-09-28 13:14:11 +0000

    Add brands carousel blocks styles

--- js/special-offers-carousel.js
+++ js/special-offers-carousel.js
@@ -1,13 +1,13 @@
 (function() {
 
     const brandSlides = [
-        '<div><img src="img/applaws.png" alt="Applaws logo"></div>',
-        '<div><img src="img/docas.png" alt="Docas logo"></div>',
-        '<div><img src="img/felix.png" alt="Felix logo"></div>',
-        '<div><img src="img/gourmet.png" alt="Gourmet logo"></div>',
-        '<div><img src="img/purizon.png" alt="Purizon logo"></div>',
-        '<div><img src="img/rocco.png" alt="Rocco logo"></div>',
-        '<div><img src="img/tigerino.png" alt="Tigerino logo"></div>',
+        '<div><img class="brand_img" src="img/applaws.png" alt="Applaws logo"></div>',
+        '<div><img class="brand_img" src="img/docas.png" alt="Docas logo"></div>',
+        '<div><img class="brand_img" src="img/felix.png" alt="Felix logo"></div>',
+        '<div><img class="brand_img" src="img/gourmet.png" alt="Gourmet logo"></div>',
+        '<div><img class="brand_img" src="img/purizon.png" alt="Purizon logo"></div>',
+        '<div><img class="brand_img" src="img/rocco.png" alt="Rocco logo"></div>',
+        '<div><img class="brand_img" src="img/tigerino.png" alt="Tigerino logo"></div>',
     ];
 
     let currentBrandSlideIdx = 0;
