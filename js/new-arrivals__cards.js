a28fb78875b3d16ff17fba260e9c31a57c8337e4
Oleksandra Shkoda <shkoda.aleksandra@gmail.com>
2022-09-28 12:47:08 +0100

    caroussel new arrivals in work

--- js/new-arrivals__cards.js
+++ js/new-arrivals__cards.js
@@ -63,4 +63,26 @@
   
     renderProducts(products);
   })();
+
+
+  (function () {
+
+    let currentSlideIndx = 0;
+ 
+    const slideContainer = document.querySelector(".arrivals_cards_container");
+ 
+    function renderSlide() {
+       slideContainer.innerHTML = productsContainer[currentSlideIndx];
+    }
+    
+    function nextSlide(){
+       currentSlideIndx = currentSlideIndx+1 >= productsContainer.length?currentSlideIndx = 0:currentSlideIndx++ ;
+       renderSlide;
+    }
+ 
+    renderSlide();
+ 
+    setInterval(renderSlide,2000);
+ 
+ })();
   
---
+++ js/new-arrivals__caroussel.js
@@ -0,0 +1,30 @@
+// (function () {
+//    for(const slides of productsContainer){
+//     slides =[...productsContainer];
+//     return console.log(slides);
+//    }
+   
+       
+     
+    
+// })()
+(function () {
+
+   let currentSlideIndx = 0;
+
+   const slideContainer = document.querySelector(".arrivals_cards_container");
+
+   function renderSlide() {
+      slideContainer.innerHTML = slides[currentSlideIndx];
+   }
+   
+   function nextSlide(){
+      currentSlideIndx = currentSlideIndx >= slides.length?currentSlideIndx = 0:currentSlideIndx++ ;
+      renderSlide;
+   }
+
+   renderSlide();
+
+   setInterval(renderSlide,2000);
+
+})();
