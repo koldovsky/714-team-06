b8b7f174a1fa559859ec89e19e91c2acfeee6aeb
maksimsemikin <maksimsemikin0520@gmail.com>
2022-09-29 15:26:22 +0300

    -added a function so that the menu collapses when the button is pressed

--- js/burger.js
+++ js/burger.js
@@ -1,19 +1,28 @@
 const menu = document.querySelector(".nav__menuburgercontent");
-const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+const menuItems = document.querySelectorAll(".menu__headerburgeritem");
+const burgerBtn = document.querySelector(".headerburgerbutton");
+const closeIcon= document.querySelector(".burgerclosedmenu");
+const menuIcon = document.querySelector(".burgeropenicon");
+const bodyBurger = document.querySelector("body")
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
+
   } else {
     menu.classList.add("showMenu");
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
+    
   }
 }
 
-hamburger.addEventListener("click", toggleMenu);
+burgerBtn.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+    function(menuItem) { 
+      menuItem.addEventListener("click", toggleMenu);
+    }
+  )
