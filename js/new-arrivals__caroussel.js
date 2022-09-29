35d54dfd1977ac0c34314b20af93516ae9da9a25
Oleksandra Shkoda <shkoda.aleksandra@gmail.com>
2022-09-29 23:48:56 +0100

    new arrivals names hover

--- js/new-arrivals__caroussel.js
+++ js/new-arrivals__caroussel.js
@@ -1,27 +1,22 @@
-(function (){
-        
+  
     let position = 0;
-    const slidesToShow = 4;
+    const slidesToShow = 1;
     const slidesToScroll = 1;
 
-    const slides_container = document.querySelectorAll('.arrivals_cards_container');
+    const slidesContainer = document.querySelectorAll('.arrivals_cards_container');
     const track = document.querySelector('.slider-track');
     const item  = document.querySelectorAll('.arrivals_cards');
     const btnPrev = document.querySelector('.btn-previous-caroussel');
     const btnNext = document.querySelector('.btn-next-caroussel');
-    const itemWidth = slides_container.width / slidesToShow;
-    const movePosition = slidesToScroll * itemWidth;
+    const movePosition = slidesToScroll * 30;
 
     btnPrev.addEventListener('click', prevMove);
     btnNext.addEventListener('click', nextMove);
 
     function prevMove(){
-        position--;
-        track.css( {transform: `translateX(${position}px)`})
+        slidesContainer.scrollRight += 30;
     };
     
     function nextMove(){
-        position++;
-        track.css( {transform: `translateX(${position}px)`})
+        slidesContainer.scrollLeft += 30;
     };
-})()
