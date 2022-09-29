1ea1ffbae79cae0568a69dc26dfb56019effb293
AnnaSokoltsova <annivawork@gmail.com>
2022-09-29 21:57:35 +0300

    Added weather forecast section
    - weather forecast section added to index.html
    - weather-forecast.js using fetch api added
    - weather-forecast styled in trending-products.css

---
+++ js/weather-forecast.js
@@ -0,0 +1,22 @@
+(function () { 
+const button = document.querySelector('.weather_button');
+const input = document.querySelector('.weather_city');
+const name = document.querySelector('.weather-display__city');
+const descr = document.querySelector('.weather-city__descr');
+const temperature = document.querySelector('.weather-city__temp');
+const wind = document.querySelector('.weather-city__wind');
+
+async function weatherForecast() {
+    try { const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=a35953c9f49cb5cf86a0efcde9ee418c');
+    let weather = await response.json();
+    console.log(weather);
+    name.innerHTML = "City: " + weather['name'];
+    descr.innerHTML = "Sky: " + weather['weather'][0]['description'];
+    temperature.innerHTML = "Temperature: " + Math.round((weather['main']['temp'] - 273.15)) + "°C";
+    wind.innerHTML = "Wind: " + weather['wind']['speed'] + " m/s";
+    } catch (err) {
+        alert("There is no such city name");
+    }
+}
+button.addEventListener('click', weatherForecast);
+})();
