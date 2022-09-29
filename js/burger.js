a91693b5a90258575b25d85d004814d3a5d46019
maksimsemikin <maksimsemikin0520@gmail.com>
2022-09-29 20:24:26 +0300

    -Added style menu button

--- js/burger.js
+++ js/burger.js
@@ -3,18 +3,23 @@ const menuItems = document.querySelectorAll(".menu__headerburgeritem");
 const burgerBtn = document.querySelector(".headerburgerbutton");
 const closeIcon= document.querySelector(".burgerclosedmenu");
 const menuIcon = document.querySelector(".burgeropenicon");
-const bodyBurger = document.querySelector("body")
+const mainBurgerHide = document.querySelector("main");
+const footerBurgerHide = document.querySelector("footer");
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
+    footerBurgerHide.style.display = "block";
+    mainBurgerHide.style.display = "block";
 
   } else {
     menu.classList.add("showMenu");
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
+    footerBurgerHide.style.display = "none";
+    mainBurgerHide.style.display = "none";
     
   }
 }
