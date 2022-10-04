7796c3db8a20a977f53e9e45b83949c208943394
unknown <vsurmaj510@gmail.com>
2022-10-04 12:41:10 +0300

    aded countdown in pets

---
+++ js/care-pets-countdown.js
@@ -0,0 +1,30 @@
+(function () {
+    const countdown = () => {
+        const countDate = new Date("October 30, 2022 00:00:00").getTime();
+        const curentTime = new Date().getTime();
+        const gap = countDate - curentTime;
+
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        const countDay = Math.floor(gap / day);
+        const countHour = Math.floor((gap % day) / hour);
+        const countMinute = Math.floor((gap % hour) / minute);
+        const countSecond = Math.floor((gap % minute) / second);
+
+        document.getElementById('body__countdown-day').innerText = countDay;
+        document.getElementById('body__countdown-hours').innerText = countHour;
+        document.getElementById('body__countdown-minutes').innerText = countMinute;
+        document.getElementById('body__countdown-seconds').innerText = countSecond;
+
+        if (gap < 1000) {
+            document.getElementById('body__countdown-day').innerText = "0";
+            document.getElementById('body__countdown-hours').innerText = "0";
+            document.getElementById('body__countdown-minutes').innerText = "0";
+            document.getElementById('body__countdown-seconds').innerText = "0";
+        }
+    }
+    setInterval(countdown, 1000);
+})();
