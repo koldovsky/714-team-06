57d30977add8728e2a88deb6d226131501b7c8b8
Oleksandra Shkoda <shkoda.aleksandra@gmail.com>
2022-10-05 22:44:59 +0100

    new arrivals and product pages format

--- js/new-arrivals__cards.js
+++ js/new-arrivals__cards.js
@@ -1,14 +1,15 @@
-  ( async function () {
+(async function () {
+  await fetch("../api/new-arrivals-products.json")
+    .then((res) => res.json())
+    .then((data) => renderProducts(data));
 
-    await fetch('../api/new-arrivals-products.json')
-    .then(res => res.json())
-    .then(data =>renderProducts(data));
-
-    function renderProducts(products) {
-     const productsContainer = document.querySelector(".arrivals_cards_container");
-      productsContainer.innerHTML = " ";
-      for (const product of products) {
-        productsContainer.innerHTML += `    
+  function renderProducts(products) {
+    const productsContainer = document.querySelector(
+      ".arrivals_cards_container"
+    );
+    productsContainer.innerHTML = " ";
+    for (const product of products) {
+      productsContainer.innerHTML += `    
   <article class="arrivals_cards">
       <a href="product.html"><img class="new_arrivals_photos"
               src=${product.img} alt="${product.title}" width="329"></a>
@@ -16,29 +17,23 @@
       <p class="price">$${product.price}</p>
       <button class="button" type="submit">Add to Card</button>
   </article>`;
-      }
     }
-     
-    const slidesContainer = document.querySelector('.arrivals_cards_container');
-    const btnPrev = document.querySelector('.btn-previous-caroussel');
-    const btnNext = document.querySelector('.btn-next-caroussel');
-    const item  = document.querySelector('.arrivals_cards');
-    console.log(item);
-    const itemWidth = item.getBoundingClientRect().width + 60;
-  
-    btnPrev.addEventListener('click', prevMove);
-    btnNext.addEventListener('click', nextMove);
+  }
 
-    function prevMove(){
-        slidesContainer.scrollLeft -= itemWidth; 
-    }
-    
-    function nextMove(){
-        slidesContainer.scrollLeft += itemWidth;
-    }
+  const slidesContainer = document.querySelector(".arrivals_cards_container");
+  const btnPrev = document.querySelector(".btn-previous-caroussel");
+  const btnNext = document.querySelector(".btn-next-caroussel");
+  const item = document.querySelector(".arrivals_cards");
+  const itemWidth = item.getBoundingClientRect().width + 60;
+
+  btnPrev.addEventListener("click", prevMove);
+  btnNext.addEventListener("click", nextMove);
+
+  function prevMove() {
+    slidesContainer.scrollLeft -= itemWidth;
+  }
+
+  function nextMove() {
+    slidesContainer.scrollLeft += itemWidth;
+  }
 })();
-
-  
-
-  
-  
--- js/products-page_quantity.js
+++ js/products-page_quantity.js
@@ -1,21 +1,20 @@
 (function (){
-    const btnPlus = document.querySelector('.bt_plus');
-    const btnMinus = document.querySelector('.bt_minus');
-    const inpt = document.querySelector('.quantity');
-    
-    function funcAdd (){
-        inpt.value = +inpt.value + 1;
-    }
+const btnPlus = document.querySelector(".bt_plus");
+const btnMinus = document.querySelector(".bt_minus");
+const inpt = document.querySelector(".quantity");
 
-    function funcExstr(){
-        const value = parseInt(inpt.value);
-       
-        if ( value > 0) {
-            inpt.value = +inpt.value - 1;
-        } 
-    }
+function funcAdd() {
+  inpt.value = +inpt.value + 1;
+}
 
-    btnPlus.addEventListener('click', funcAdd);
-    btnMinus.addEventListener('click' , funcExstr);
+function funcExstr() {
+  const value = parseInt(inpt.value);
 
+  if (value > 0) {
+    inpt.value = +inpt.value - 1;
+  }
+}
+
+btnPlus.addEventListener("click", funcAdd);
+btnMinus.addEventListener("click", funcExstr);
 })();
