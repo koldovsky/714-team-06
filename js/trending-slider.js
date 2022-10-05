9557b87a05f95c6321f2b990dbac06e040e83b5a
AnnaSokoltsova <annivawork@gmail.com>
2022-10-05 20:01:41 +0300

    trending-products slider
    added fetch api

--- js/trending-slider.js
+++ js/trending-slider.js
@@ -1,26 +1,4 @@
-// (function () { 
-// const productContainer = document.querySelector('.trending-products__items'); 
-// const itemWidth = document.querySelector('.trending-products__item').getBoundingClientRect().width;
-// const nxtBtn = document.querySelector('.trending-products__arrow-img-right');
-// const preBtn = document.querySelector('.trending-products__arrow-img-left');
-// const nxtBtnSmall = document.querySelector('.trending-products__sm-arrow-right');
-// const preBtnSmall = document.querySelector('.trending-products__sm-arrow-left');
-
-// nxtBtnSmall.addEventListener('click', () => {
-//     productContainer.scrollLeft += itemWidth + 30;
-// });
-// preBtnSmall.addEventListener('click', () => {
-//     productContainer.scrollLeft -= itemWidth + 30;
-// });
-// nxtBtn.addEventListener('click', () => {
-//     productContainer.scrollLeft += itemWidth + 30;
-// });
-// preBtn.addEventListener('click', () => {
-//     productContainer.scrollLeft -= itemWidth + 30;
-// });
-// })();
-
-$(document).ready(function() {
+function sliderTrending() {
     $('.trending-products__items').slick({
         arrows: true,
         adaptiveHeight: true,
@@ -37,13 +15,13 @@ $(document).ready(function() {
                 }
               },
             {
-              breakpoint: 1024,
+              breakpoint: 991,
               settings: {
                 slidesToShow: 4,
                 slidesToScroll: 1
               }
             }
         ]
-
     });
-});
+}
+
