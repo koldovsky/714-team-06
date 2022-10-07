9e34a0c3e7ca93d418898c210921b61bfbaf09dd
MishaStankovich <mishastankovich199@gmail.com>
2022-10-07 15:35:54 +0000

    add carousel

--- js/any-products.js
+++ js/any-products.js
@@ -8,9 +8,31 @@ $(document).ready(function() {
     const btnPrev = $('.btn-prev');
     const btnNext = $('.btn-next');
     const itemWidth = carousel.width() / slidesToShow;
+    const movePosition = slidesToScroll * itemWidth;
+
     hamster.each(function(index, hamster){
         $(hamster).css({
             minWidth: itemWidth,
-        })
-    })
+        });
+    });
+    
+
+   
+    btnNext.click (function() {
+        position -= movePosition
+    
+
+    track.css({
+        transform: `translateX(${position}px)`
+    });
+    });
+
+    btnPrev.click (function() {
+        position += movePosition
+    
+
+    track.css({
+        transform: `translateX(${position}px)`
+    });
+    });
 });
