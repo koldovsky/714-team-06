5113d450c358f5202d26059b4378cf3f9caf4e9a
MishaStankovich <mishastankovich199@gmail.com>
2022-10-13 13:02:52 +0000

    still make slider

--- js/any-products.js
+++ js/any-products.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     let position = 0;
     const slidesToShow = 4;
     const slidesToScroll = 1;
@@ -49,10 +49,48 @@ $(document).ready(function() {
         );
     };*/
 
-    $('.slider-track').slick({
+   /* $('.slider-track').slick({
       
       slidesToShow: 4,
       slidesToScroll: 1,
       });
     checkButtons();
-});
+});*/
+
+$('.sliders-track').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+    
+  });
